9d43b445edaa257dc98795df2289488c5c056a0b
rus42-hub <ruslana.budniak@edu.univ-fcomte.fr>
2025-11-25 16:26:50 +0100

    Commit 2 : supprimer marker par défaut pour summary

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,11 @@
 // *** ACCORDEON ***
+const accord = document.querySelector(".accordeon");
+
+accord.querySelectorAll("details").forEach(det => {
+    det.addEventListener("click", () => {
+        accord.querySelectorAll("details").forEach(d => {
+            if (d !== det) d.open = false;
+        });
+    });
+});
+
